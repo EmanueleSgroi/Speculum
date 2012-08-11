2caae61c8cc4eb20ae4bf35c4ff1a50e2781b192
Emanuele Sgroi <emanuele.sgroi@gmail.com>
2012-08-11 02:23:55 +0200

    Ansi C compatible.

--- lib/speculum.c
+++ lib/speculum.c
@@ -5,20 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock.h>
-
-#define MAX_CONN 10
-#define PORT 1337
-
-typedef struct {
-	int *num_clients;
-	int *first;
-	int *len_buf;
-	char buffer[200];
-	SOCKET *clients;
-	SOCKET *ptr;
-	struct sockaddr client_addr;
-} Clients;
-Clients clients;
+#include "speculum.h"
 
 SOCKET initialize()
 {
